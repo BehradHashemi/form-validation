6ac28b4e9662dad45139d6f8e948fccc928fc338
Behrad07 <behradweb07@gmail.com>
2023-04-01 19:26:17 +0430

    Start validation with JS and select required tags

---
+++ app.js
@@ -0,0 +1,9 @@
+const mailInput = document.querySelector(".name-input")
+const errorMail = document.querySelector(".valid-name")
+
+const passInput = document.querySelector(".pass-input")
+const errorPass = document.querySelector(".valid-pass")
+
+const errorData = document.querySelector(".valid-data")
+
+const btn = document.querySelector("#submit")
