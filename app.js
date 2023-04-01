16907ff7b19a79b7b2f91b6adee114befca31508
Behrad07 <behradweb07@gmail.com>
2023-04-01 19:34:24 +0430

    Validation was successful, but sending it to the server remains

--- app.js
+++ app.js
@@ -7,3 +7,37 @@ const errorPass = document.querySelector(".valid-pass")
 const errorData = document.querySelector(".valid-data")
 
 const btn = document.querySelector("#submit")
+
+function valid(event) {
+    event.preventDefault();
+    const mailInputValue = mailInput.value;
+    const passInputValue = passInput.value;
+    const pattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,3})$/;
+    let ifSendData = true;
+
+    if (mailInputValue.length === 0) {
+        errorMail.innerHTML = "👆Enter your E-mail";
+        mailInput.style.borderBottom = " 1px solid #ff0000";
+        ifSendData = false;
+    } else if (pattern.test(mailInputValue) === false) {
+        errorMail.innerHTML = "👆Enter your email correctly";
+        mailInput.style.borderBottom = " 1px solid #ff0000";
+        ifSendData = false;
+    } else {
+        errorMail.innerHTML = "";
+        mailInput.style.borderBottom = " 1px solid #fff";
+    }
+
+    if (passInputValue.length === 0) {
+        errorPass.innerHTML = "👆Enter your password";
+        passInput.style.borderBottom = " 1px solid #ff0000";
+        ifSendData = false;
+    } else if (passInputValue.length < 4) {
+        errorPass.innerHTML = "👆Your password is short";
+        passInput.style.borderBottom = " 1px solid #ff0000";
+        ifSendData = false;
+    } else {
+        errorPass.innerHTML = "";
+        passInput.style.borderBottom = "1px solid #fff";
+    }
+}
